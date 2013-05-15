829746681c75a414213aa03a1609c7686b1887ea
Malte Bublitz <me@malte-bublitz.de>
2013-05-15 16:01:53 +0200

    fixed issues made while merging...

--- log.py
+++ log.py
@@ -13,31 +13,6 @@ ERROR=42
 QUIET=666
 
 class Log(object):
-<<<<<<< HEAD
-	def __init__(self, filename):
-		if not os.access(os.path.join(os.path.dirname(__self__), filename), W_OK):
-			print "Log file does not exists. Creating one for you."
-			f = file(filename, 'w')
-			f.close()
-			print "Logfile saved!"
-		self.file = filename
-	
-	def info(self, text, notify):
-		now = datetime.now()
-		msg = str("[" + now.month + "/" + now.day + "/" + now.year + " " + now.hour + ":" + now.minute + ":" + now.second + "] [INFO] " + text)
-		with open(file, "w") as my_file:
-			my_file.write(msg)
-		if notify:
-			print msg
-		
-	def error(self, text, notify):
-		now = datetime.now()
-		msg = str("[" + now.month + "/" + now.day + "/" + now.year + " " + now.hour + ":" + now.minute + ":" + now.second + "] [ERROR] " + text)
-		with open(file, "w") as my_file:
-			my_file.write(msg)
-		if notify:
-			print msg
-=======
 	
 	loglevel_file = INFO
 	loglevel_console = ERROR
@@ -69,4 +44,3 @@ class Log(object):
 			self.logfile.write(msg +"\n")
 		if self.loglevel_console <= ERROR:
 			print msg
->>>>>>> 740263c88b23250c1f16878fc4ad81bed9d157c9
